aa5ddea5ea6b52bd0ed84593bf2427c67863a469
RafidRisun <rafidhassanrisun@gmail.com>
2025-09-04 07:00:05 +0600

    Fixed Photo URL

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -10,6 +10,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [role, setRole] = useState("");
+  const [photo_url, setPhotoUrl] = useState("");
   const [specialization, setSpecialization] = useState("");
   const [specializations, setSpecializations] = useState<string[]>([]);
   // Error state for validation
@@ -80,8 +81,8 @@ export default function Login() {
 
     const payload =
       role === "DOCTOR"
-        ? { name, email, password, specialization }
-        : { name, email, password };
+        ? { name, email, password, specialization, photo_url }
+        : { name, email, password, photo_url };
 
     // Send registration request
     axiosInstance.post(endpoint, payload).then((res) => {
@@ -247,6 +248,8 @@ export default function Login() {
             type="url"
             name="photo_url"
             placeholder="Photo URL (optional)"
+            value={photo_url}
+            onChange={(e) => setPhotoUrl(e.target.value)}
             className="flex border border-gray-500 rounded-lg px-4 py-2 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           {/* Submit button */}

70fe09bb9dabc87814cd601336e00e45ed8c3cb5
RafidRisun <rafidhassanrisun@gmail.com>
2025-09-04 08:31:01 +0600

    Auto Scroll to top on page change added

--- app/components/pagination.tsx
+++ app/components/pagination.tsx
@@ -11,7 +11,10 @@ export default function Pagination({ setPage, currentPage }: PaginationProps) {
     <div className="flex flex-row gap-5 justify-between">
       <button
         className="flex justify-start w-full bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-600 cursor-pointer"
-        onClick={() => setPage((currentPage) => Math.max(currentPage - 1, 1))}
+        onClick={() => {
+          setPage((currentPage) => Math.max(currentPage - 1, 1));
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
       >
         <div className="flex flex-row gap-2 items-center">
           <MdOutlineNavigateNext className="transform rotate-180 text-2xl" />
@@ -20,7 +23,10 @@ export default function Pagination({ setPage, currentPage }: PaginationProps) {
       </button>
       <button
         className="flex justify-end w-full bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-600 cursor-pointer"
-        onClick={() => setPage((currentPage) => currentPage + 1)}
+        onClick={() => {
+          setPage((currentPage) => currentPage + 1);
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
       >
         <div className="flex flex-row gap-2 items-center">
           <span>Next</span>
